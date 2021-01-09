569a174b2d14b25aede831f337237908f9bd9b77
Patrick Spitzer <rick.spitzer@yahoo.com>
2021-01-09 16:54:54 -0700

    wrote function for regressions and tested

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "61f14dfec59cd0cb00789313e43c1124"
+weather_api_key = "YOUR KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyDzyJcx9YdcqpTrSOqRCi64UKe-okrkKxI"
+g_key = "YOUR KEY HERE"
